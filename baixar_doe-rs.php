0d18da83c6bd5739aba790487e7868b3fb869cf7
amg1127 <amg1127@5465fe1f-e9f4-4a51-a9b0-42098c041c11>
2011-10-31 17:16:28 +0000

    Script "baixar_doe-rs.php" adaptado para a instalação de Joomla! do novo site da CORAG.

--- baixar_doe-rs.php
+++ baixar_doe-rs.php
@@ -23,9 +23,9 @@ if ($_SERVER['argc'] < 2) {
 } else {
     for ($i = 1; $i < $_SERVER['argc']; $i++) {
         if (preg_match ("/^(\\d\\d)\\s*\\/\\s*(\\d\\d)\\s*\\/\\s*(\\d\\d\\d\\d)\$/", trim ($_SERVER['argv'][$i]), $matches)) {
-            $datas_baixar_doe[] = $matches[1] . "/" . $matches[2] . "/" . $matches[3];
+            $datas_baixar_doe[] = $matches[3] . "-" . $matches[2] . "-" . $matches[1];
         } else if (preg_match ("/^(\\d\\d\\d\\d)(\\d\\d)(\\d\\d)\$/", trim ($_SERVER['argv'][$i]), $matches)) {
-            $datas_baixar_doe[] = $matches[3] . "/" . $matches[2] . "/" . $matches[1];
+            $datas_baixar_doe[] = $matches[1] . "-" . $matches[2] . "-" . $matches[3];
         } else {
             morre ("Data invalida no argumento em linha de comando #" . $i . ": '" . $_SERVER['argv'][$i] . "'!");
         }
@@ -125,7 +125,7 @@ $logindom = new DOMDocument ("1.0", "utf-8");
 $logindom->validateOnParse = true;
 $logindom->recover = true;
 $logindom->strictErrorChecking = false;
-if (! $logindom->loadHTML ($saida)) {
+if (! @ $logindom->loadHTML ($saida)) {
     morre ("Impossivel analisar HTML da pagina de login da CORAG!");
 }
 $formul = $logindom->getElementById ("login");
@@ -153,30 +153,38 @@ for ($i = 0; $i < $inputelements->length; $i++) {
 # Efetuar login na pagina da CORAG
 $saida = baixa_arquivo ("http://www.corag.com.br/index.php?option=com_user&lang=en", "username=" . urlencode ($login) . "&passwd=" . urlencode ($senha) . $submitotherdata);
 if (! preg_match ("/<span>Logout<\\/span>/is", $saida)) {
-    morre ("Login falhou!\n");
+    morre ("Login falhou!");
 }
 
-morre ("Ainda parcial!");
-
+# Acessar a pagina onde esta o DOE
+$matches = array ();
 $saida = baixa_arquivo ("http://www.corag.com.br/index.php?option=com_jornal&view=jornais&Itemid=119&lang=en");
-if (! preg_match ("/<a\\s+href=['\"]?index\\.php\\?option=com_jornal&[^>'\"]+['\"]?[^>]*>\\s*Di(&aacute;|a|�)rio\\s+Oficial\\s+do\\s+RS\\s*<\\/a>/is", $saida)) {
+if (! preg_match ("/<a\\s+class=['\"]?link_jornal['\"]?\\s+href=['\"]?(\\/index\\.php\\?option=com_jornal&[^'\">]+)['\"]?[^>]*>\\s*Di(&aacute;|a|á)rio\\s+Oficial\\s+do\\s+RS\\s*<\\/a>/is", $saida, $matches)) {
     morre ("Impossivel encontrar diario oficial do estado!");
 }
 
-morre ("Ainda parcial!");
-
-# Acessar a pagina onde esta o DOE
-$saida = baixa_arquivo ("http://www.corag.rs.gov.br/diario/jornal.php?jornal=doe");
-if (! preg_match ("/<input\\s+type=['\"]?text['\"]?\\s+id=['\"]?edtData['\"]?\\s+name=['\"]?edtData['\"]?\\s+size=['\"]?\\d+['\"]?\\s+maxlength=['\"]?\\d+['\"]?\\s+value=['\"]?(|\\d+\\/\\d+\\/\\d+)['\"]?\\s*\\/?>/is", $saida)) {
-    morre ("Falha ao encontrar o formulario de busca por data!");
+$urlbasedoers = $matches[1];
+if (count (explode ("&amp;", $urlbasedoers)) > 1) {
+    $urlbasedoers = html_entity_decode ($urlbasedoers);
 }
+$urlbasedoers = "http://www.corag.com.br" . $urlbasedoers;
+
+$saida = baixa_arquivo ($urlbasedoers);
+if (! (stripos ($saida, "Diário Oficial do RS") && strpos ($saida, "Sumário da edição de"))) {
+    morre ("Falha ao identificar pagina do Diario Oficial do RS");
+}
+
+if (substr ($urlbasedoers, -1) != "&") {
+    $urlbasedoers .= "&";
+}
+$urlbasedoers = preg_replace ("/&data=\\d\\d\\d\\d-\\d\\d-\\d\\d&/", "&", $urlbasedoers) . "data=";
 
 # Acessar a data especificada pelo usuario
 foreach ($datas_baixar_doe as $data_doe) {
-    $data_sepa = explode ('/', $data_doe);
-    $tstmp_doe = implode ('', array_reverse ($data_sepa));
+    $data_sepa = explode ('-', $data_doe);
+    $tstmp_doe = implode ('', $data_sepa);
     $doe_output_file = $mydir . "/doe_" . $tstmp_doe . ".pdf";
-    $tstmp_unix = mktime (0, 0, 0, $data_sepa[1], $data_sepa[0], $data_sepa[2]);
+    $tstmp_unix = mktime (0, 0, 0, $data_sepa[1], $data_sepa[2], $data_sepa[0]);
     if ($tstmp_unix === false || $tstmp_unix < 0) {
         aviso ("Impossivel interpretar data '" . $data_doe . "'!");
     } else {
@@ -187,9 +195,11 @@ foreach ($datas_baixar_doe as $data_doe) {
             aviso ("'" . $data_doe . "' cai em um sabado!");
         }
     }
+    
     aviso ("Buscando DOE de '" . $data_doe . "' no site da CORAG...");
-    $saida = baixa_arquivo ("http://www.corag.rs.gov.br/diario/jornal.php?jornal=doe", "SearchString=&sel_and_or=&edtData=" . urlencode($data_doe) . "&btn_pesquisar=Localizar");
-    if (! ($cnt_matches = preg_match_all ("/<a\\s+href=['\"]?(\\.\\/controle\\.php\\?pg=\\d+)['\"]?\\s+class=['\"]?pagina['\"]?\\s*>\\s*(\\d+)\\s*<\\/a>/is", $saida, $matches, PREG_SET_ORDER))) {
+    $saida = baixa_arquivo ($urlbasedoers . urlencode($data_doe));
+
+    if (! ($cnt_matches = preg_match_all ("/<div\\s+class=['\"]?item_pagina['\"]?\\s*>\\s*<a\\s+href=['\"]?(\\/index\\.php\\?option=com_jornal&[^'>\"]+)['\"]?\\s*>\\s*(\\d+)\\s*<\\/a>\\s*<\\/div>/is", $saida, $matches, PREG_SET_ORDER))) {
         morre ("Falha ao determinar as paginas disponiveis do DOE de '" . $data_doe . "'!");
     }
     $pdfjoin_args = '';
@@ -197,7 +207,7 @@ foreach ($datas_baixar_doe as $data_doe) {
     $pg_ultimo = false;
     $pgs = array ();
     foreach ($matches as $match) {
-        if (("./controle.php?pg=" . $match[2]) != $match[1]) {
+        if (! (stripos ($match[1], "&pagina=" . $match[2] . "&") || stripos ($match[1], "&amp;pagina=" . $match[2] . "&amp;"))) {
             morre ("Inconsistencia no link '" . $match[0] . "' do DOE de '" . $data_doe . "'!");
         }
         $pg_n = intval ($match[2], 10);
@@ -210,7 +220,7 @@ foreach ($datas_baixar_doe as $data_doe) {
         if (array_key_exists ($pg_n, $pgs)) {
             morre ("A pagina '" . $match[2] . "' do DOE de '" . $data_doe . "' esta repetida!");
         } else {
-            $pgs[$pg_n] = $match[2];
+            $pgs[$pg_n] = array ($match[1], $match[2]);
         }
     }
     if ($pg_primeiro != 1) {
@@ -235,29 +245,42 @@ foreach ($datas_baixar_doe as $data_doe) {
     if (! $nao_precisa) {
         for ($pg_n = 1; $pg_n <= $pg_ultimo; $pg_n++) {
             if (array_key_exists ($pg_n, $pgs)) {
-                $n_fn = $pages_dir . "/doe_" . $tstmp_doe . "_pg" . $pgs[$pg_n] . ".pdf";
+                $n_fn = $pages_dir . "/doe_" . $tstmp_doe . "_pg" . $pgs[$pg_n][1] . ".pdf";
                 if (file_exists ($n_fn)) {
                     if (testa_mimetype ($n_fn, MIMETYPE_PDF)) {
                         $pdfjoin_args .= ' ' . escapeshellarg ($n_fn);
                         continue;
                     }
                 }
-                $saida = baixa_arquivo ("http://www.corag.rs.gov.br/diario/controle.php?pg=" . $pgs[$pg_n]);
-                if (! preg_match ("/<meta\\s+http-equiv=['\"]?refresh['\"]?\\s+content=['\"]?0;\\s*url=\\.\\/index\\.php ['\"]?\\s*>/is", $saida)) {
-                    morre ("Impossivel abrir a pagina '" . $pgs[$pg_n] . "' do DOE de '" . $data_doe . "'!");
+                $saida = baixa_arquivo ("http://www.corag.com.br" . $pgs[$pg_n][0]);
+                if (! preg_match ("/<iframe\\s+src=['\"]?((http:\\/\\/www\\.corag\\.com\\.br\\/components\\/com_jornal\\/views\\/pagina\\/tmpl\\/)index\\.php)['\"]?\\s*[^>]*>/is", $saida, $iframedoe)) {
+                    morre ("Impossivel abrir a pagina '" . $pgs[$pg_n][1] . "' do DOE de '" . $data_doe . "'!");
                 }
-                $saida = baixa_arquivo ("http://www.corag.rs.gov.br/diario/index.php");
-                if (! preg_match ("/<frame\\s+src=['\"]?\\/diario\\/superior.php['\"]?\\s+name=['\"]?superior['\"]?\\s*>/is", $saida)) {
-                    morre ("Impossivel visualizar 'frameset' da pagina '" . $pgs[$pg_n] . "' do DOE de '" . $data_doe . "'!");
+                $saida = baixa_arquivo ($iframedoe[1]);
+                if (! preg_match ("/<frame\\s+src=['\"]?superior\\.php['\"]?\\s+name=['\"]?superior['\"]?\\s+style=['\"]?z-index\\s*:\\s*\\d+\\s*;?\\s*['\"]?\\s*>/is", $saida)) {
+                    morre ("Impossivel visualizar 'frameset' da pagina '" . $pgs[$pg_n][1] . "' do DOE de '" . $data_doe . "'!");
                 }
-                baixa_arquivo ("http://www.corag.rs.gov.br/diario/superior.php", false, true);
+                baixa_arquivo ($iframedoe[2] . "superior.php", false, true);
+                // Os desenvolvedores da CORAG (ou PROCERGS) deixaram um bug na aplicacao: o PDF gerado possui caracteres estranhos no inicio do arquivo,
+                // o que invalida a assinatura do PDF. Se houver essa assinatura invalida, remove-la.
+                $arqsaida = file_get_contents ($output_file);
+                if ($arqsaida === false) {
+                    morre ("Impossivel analisar arquivo PDF baixado da pagina " . $pgs[$pg_n][1] . " do DOE de '" . $data_doe . "'!");
+                }
+                $cabec = substr ($arqsaida, 0, 58);
+                if (preg_match ("/^\\/estatico\\/diario\\/doe\\/(img|pdf)\\/\\d\\d\\d\\d\\d\\d\\d\\d\\/doe\\d\\d\\d\\d\\d\\d\\d\\d_\\d\\d\\d\\.pdf%PDF-/is", $cabec)) {
+                    $arqsaida = substr ($arqsaida, 53);
+                    if (file_put_contents ($output_file, $arqsaida) !== strlen ($arqsaida)) {
+                        morre ("Impossivel reescrever arquivo PDF baixado da pagina " . $pgs[$pg_n][1] . " do DOE de '" . $data_doe . "'!");
+                    }
+                }
                 if (testa_mimetype ($output_file, MIMETYPE_PDF)) {
                     if (! copy ($output_file, $n_fn)) {
-                        morre ("Impossivel salvar pagina '" . $pgs[$pg_n] . "' do DOE de '" . $data_doe . "' no computador!");
+                        morre ("Impossivel salvar pagina '" . $pgs[$pg_n][1] . "' do DOE de '" . $data_doe . "' no computador!");
                     }
                     $pdfjoin_args .= ' ' . escapeshellarg ($n_fn);
                 } else {
-                    morre ("A pagina '" . $pgs[$pg_n] . "' do DOE de '" . $data_doe . "' nao eh um arquivo PDF!");
+                    morre ("A pagina '" . $pgs[$pg_n][1] . "' do DOE de '" . $data_doe . "' nao eh um arquivo PDF!");
                 }
             } else {
                 aviso ("A pagina #" . $pg_n . " do DOE de '" . $data_doe . "' nao foi publicada no site da CORAG!");
